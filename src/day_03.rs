051f55a0a215696ec603e48c9df6e83a3e11ce45
Ben Gosney <bengosney@googlemail.com>
2022-12-04 11:17:23 +0000

    Bad part 2

--- src/day_03.rs
+++ src/day_03.rs
@@ -8,7 +8,7 @@ fn score(c: char) -> u32 {
     }
 }
 
-fn part_1(contents: String) -> u32 {
+fn part_1(contents: &String) -> u32 {
     contents
         .lines()
         .map(|line| {
@@ -35,11 +35,49 @@ ttgJtRGJQctTZtZT
 CrZsJsPPZsGzwwsLwLmpwMDw"
         .to_string();
 
-    assert_eq!(part_1(test_str), 157);
+    assert_eq!(part_1(&test_str), 157);
+}
+
+fn part_2(contents: &String) -> u32 {
+    let mut actual_iterator = Vec::new();
+    for line in contents.lines() {
+        actual_iterator.push(line);
+    }
+
+    let mut total = 0;
+    for lines in actual_iterator.chunks(3) {
+        let one: HashSet<char> = HashSet::from_iter(lines[0].chars());
+        let two: HashSet<char> = HashSet::from_iter(lines[1].chars());
+        let three: HashSet<char> = HashSet::from_iter(lines[2].chars());
+
+        let inter_hash: HashSet<char> =
+            HashSet::from_iter(one.intersection(&two).map(|c| c.clone()));
+
+        total += inter_hash
+            .intersection(&three)
+            .map(|c: &char| score(*c))
+            .sum::<u32>();
+    }
+
+    return total;
+}
+
+#[test]
+fn test_part_2() {
+    let test_str: String = "vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw"
+        .to_string();
+
+    assert_eq!(part_2(&test_str), 70);
 }
 
 pub fn run(input: String) -> (u32, u32) {
-    let part_1 = part_1(input);
+    let part_1: u32 = part_1(&input);
+    let part_2: u32 = part_2(&input);
 
-    (part_1, 1)
+    (part_1, part_2)
 }
